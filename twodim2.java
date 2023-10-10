8b67c26fb4ce64797f15d78c98552cd27f3ba93b
Himali-Silwadiya <143890058+Himali-Silwadiya@users.noreply.github.com>
2023-10-10 22:26:07 +0530

    twodim2.java

---
+++ twodim2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.*;
+public class TwoDimen
+{
+	public static void main(String[] args) {
+	    Scanner sc =new Scanner(System.in);
+	    String [][] salutation={{"Mr.","Mrs.","Ms."},{"Kumar"}};
+	    System.out.println(salutation[0][0]+" "+salutation[1][0]);
+	    System.out.println(salutation[0][1]+" "+salutation[1][0]);
+	    System.out.println(salutation[0][2]+" "+salutation[1][0]);
+	}
+}
