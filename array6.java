e2993febeecbed28375c40942c8b7af4f88a759d
Himali-Silwadiya <143890058+Himali-Silwadiya@users.noreply.github.com>
2023-10-10 20:12:18 +0530

    array6.java

---
+++ array6.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    int a[]={10,30,2,5,6,7};
+		Arrays.sort(a);
+		System.out.println("Max is"+a[a.length-1]);
+		System.out.println("Min is"+a[0]);
+	}
+}
