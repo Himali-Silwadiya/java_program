711e446c696d6447a979b061d4c23bd15bd664b0
Himali-Silwadiya <143890058+Himali-Silwadiya@users.noreply.github.com>
2023-11-02 21:56:27 +0530

    constructor chaining.java

---
+++ constructor chaining.java
@@ -0,0 +1,17 @@
+class Main{
+    int sum;
+    Main(){
+        this(5,2);
+    }
+    Main(int a1,int a2){
+        this.sum=a1+a2;
+    }
+    void display()
+    {
+        System.out.println("Sum is"+sum);
+    }
+    public static void main(String args[]){
+        Main m=new Main();
+        m.display();
+    } 
+}
