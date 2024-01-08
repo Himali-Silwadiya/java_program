6e11e7252d5a9aa1c9d759af12a3ee79d900071f
Himali-Silwadiya <143890058+Himali-Silwadiya@users.noreply.github.com>
2024-01-08 12:44:04 +0530

    Multiple thread.java

---
+++ Multiple thread.java
@@ -0,0 +1,18 @@
+//Implementation of multiple thread using Runnable class
+public class Main implements Runnable
+{
+	public static void main(String[] args) {
+	    Main thread1=new Main();
+	    Thread t1=new Thread(thread1);
+	    t1.start();
+	    Main thread2=new Main();
+	    
+	    Thread t2=new Thread(thread2);
+	    t2.start();
+	   
+	    System.out.println("This code is outside of the thread");
+	}
+	public void run(){
+		System.out.println("the code is running in a thread");
+	}
+}
